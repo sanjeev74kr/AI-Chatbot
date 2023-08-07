07c87b508856d49d2a826c94c17375fc30b19401
Sanjeev Kumar <san74kr@gmail.com>
2023-08-08 01:13:09 +0530

    Make Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import chats from './components/Chats'
+import { useState } from 'react';
 
 function App() {
+  const[showChats,setShowChats]=useState(false);
+
+ const handleChatClick=()=>{
+    setShowChats(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showChats && <chats />}
+      <div className='Heading-part'>
+        <span className='brain-wave-icon'>
+          <img src='./brain.svg' alt='brain-wave-icon' />
+        </span>
+        <span className='brainwave-text'>
+          <h1>Brainwave</h1>
+        </span>
+      </div>
+      <div className='Main-page-side-section'>
+        <div className='Menu'>
+          <div className='Menu-item' id="Menu-chats" onClick={handleChatClick}>
+            <span className="icon"><img src='./chat.svg' alt='brain-wave-icon' /></span>
+            <span className="text">Chats</span>
+          </div>
+          <div className='Menu-item' id="Menu-search">
+            <span className="icon"><img src='./search.svg' alt='brain-wave-icon' /></span>
+            <span className="text">Search</span>
+          </div>
+          <div   className='Menu-item' id="Menu-subscription">
+            <span className="icon"><img src='./subscription.svg' alt='brain-wave-icon' /></span>
+            <span className="text">Manage Subscription</span>
+          </div>
+          <div className='Menu-item' id="Menu-update">
+            <span className="icon"><img src='./faq.svg' alt='brain-wave-icon' /></span>
+            <span className="text">Update & FAQ</span>
+          </div>
+          <div className='Menu-item' id="Menu-setting">
+            <span className="icon"><img src='./settings.svg' alt='brain-wave-icon' /></span>
+            <span className="text">Settings</span>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/Chats.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function chats(){
+    return (
+        <div className="Chats">
+            <h1>Unlock the Power of AI</h1>
+            <h3>Chat with the smartest AI - Experience the power of AI with us</h3>
+        </div>
+    )
+}
+
+export default chats;
