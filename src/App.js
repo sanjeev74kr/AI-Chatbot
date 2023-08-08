9c7f972c9aecc9a393a54514f03957c49bf87696
Sanjeev Kumar <san74kr@gmail.com>
2023-08-08 15:13:43 +0530

    Make chat conversation part

--- src/App.js
+++ src/App.js
@@ -1,50 +1,60 @@
 import './css/App.css';
-import chats from './components/Chats'
+import Chats from './components/Chats'
 import { useState } from 'react';
 
 function App() {
-  const[showChats,setShowChats]=useState(false);
+  const [showChats, setShowChats] = useState(true);
+  const [showSearch, setShowSearch] = useState(false);
+  const [showSubscription, setShowSubscription] = useState(false);
+  const [showFaq, setShowFaq] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
- const handleChatClick=()=>{
+  const handleChatClick = () => {
     setShowChats(true);
   }
 
   return (
     <div className="App">
-      {showChats && <chats />}
       <div className='Heading-part'>
-        <span className='brain-wave-icon'>
-          <img src='./brain.svg' alt='brain-wave-icon' />
+        <span className='DI-icon'>
+          <img src='./brain.svg' alt='DI-icon' />
         </span>
-        <span className='brainwave-text'>
-          <h1>Brainwave</h1>
+        <span className='DI-text'>
+          <h1> D I</h1>
         </span>
       </div>
+    //  <div className='Vertical-line'></div>
       <div className='Main-page-side-section'>
         <div className='Menu'>
-          <div className='Menu-item' id="Menu-chats" onClick={handleChatClick}>
+          <div className={`Menu-item ${showChats ? 'selected' : ''}`} id="Menu-chats" onClick={handleChatClick}>
             <span className="icon"><img src='./chat.svg' alt='brain-wave-icon' /></span>
             <span className="text">Chats</span>
           </div>
-          <div className='Menu-item' id="Menu-search">
+          <div className={`Menu-item ${showSearch ? 'selected' : ''}`} id="Menu-search">
             <span className="icon"><img src='./search.svg' alt='brain-wave-icon' /></span>
             <span className="text">Search</span>
           </div>
-          <div   className='Menu-item' id="Menu-subscription">
+          <div className={`Menu-item ${showSubscription ? 'selected' : ''}`} id="Menu-subscription">
             <span className="icon"><img src='./subscription.svg' alt='brain-wave-icon' /></span>
             <span className="text">Manage Subscription</span>
           </div>
-          <div className='Menu-item' id="Menu-update">
+          <div className={`Menu-item ${showFaq ? 'selected' : ''}`} id="Menu-update">
             <span className="icon"><img src='./faq.svg' alt='brain-wave-icon' /></span>
             <span className="text">Update & FAQ</span>
           </div>
-          <div className='Menu-item' id="Menu-setting">
+          <div className={`Menu-item ${showSettings ? 'selected' : ''}`} id="Menu-setting">
             <span className="icon"><img src='./settings.svg' alt='brain-wave-icon' /></span>
             <span className="text">Settings</span>
           </div>
         </div>
-        
+      //<div className='Horizontal-line'></div>
       </div>
+
+      <div className="Chat-component">
+    {showChats && <Chats />}
+  </div>
+    
+
     </div>
   );
 }
--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,81 @@
 import React from "react";
+import '../css/Chats.css'
+import { useState } from "react";
 
-function chats(){
+function Chats() {
+    const [inputValue, setInputValue] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSendMessage = () => {
+        if (inputValue.trim() === '') return;
+
+        const newMessage = {
+            text: inputValue,
+            isUser: true,
+        };
+
+        setMessages([...messages, newMessage]);
+        setInputValue('');
+
+
+        setTimeout(() => {
+            const conversationContainer = document.querySelector(".chatbot-conversation");
+            conversationContainer.scrollTop = conversationContainer.scrollHeight;
+        }, 10);
+
+    }
+
     return (
-        <div className="Chats">
-            <h1>Unlock the Power of AI</h1>
-            <h3>Chat with the smartest AI - Experience the power of AI with us</h3>
-        </div>
+        <div className="Chats-UI">
+            <div className="Chats-section">
+                <div className="Chats-heading">
+                    <div className="Chats-heading1">
+                        <h1 >Unlock the Power of AI</h1>
+                    </div>
+                    <div className="Chats-heading2">
+                        <h5>Chat with the smartest AI - Experience the power of AI with us</h5>
+                    </div>
+                </div>
+                <div className="Chats-conversation">
+                    <div className="message bot">Hi! I am Documnets Intelligence 😄</div>
+                    <div className="message bot">Tell me, How can I help You</div>
+
+                    {messages.map((message, index) => (
+                        <div key={index} className={`message ${message.isUser ? 'user' : 'bot'}`}>
+                            {message.text}
+                        </div>
+                    ))};
+                </div>
+
+            </div>
+            <div className="Chats-input-box">
+                <input type="text" className="input-box" placeholder="Ask Documnets Intelligence anything..."
+                    value={inputValue}
+                    onChange={handleInputChange} />
+
+                <button className="send-button" onClick={handleSendMessage}>
+                    <img src="send.svg" alt="Send Icon" />
+                </button>
+
+            </div>
+        
+             {/* <div className="Chats-history-section">
+                <div className="Chat-history-top">
+                    <span className="Notifcation"></span>
+                    <span className="Profile"></span>
+                    <span className="Share"></span>
+                </div>
+                <div className="Chat-history"><h3>Search History</h3></div>
+                <div className="New-chat">
+                    <button className="New-chat-button">New Chat</button>
+                </div>
+            </div> */}
+        </div >
     )
 }
 
-export default chats;
+export default Chats;
