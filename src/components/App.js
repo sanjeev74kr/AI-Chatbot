54319b1ae135a3d8b1b4f626f6726afb28e87d10
Sanjeev Kumar <san74kr@gmail.com>
2023-08-10 14:23:13 +0530

    Modified left section of the page to load list of industry

--- src/components/App.js
+++ src/components/App.js
@@ -6,76 +6,54 @@ import sampleData from '../utils/sampleData'
 
 function App() {
   const [showChats, setShowChats] = useState(true);
-  const [data,setData]=useState(sampleData);
+  const [data, setData] = useState(sampleData);
 
 
   const handleChatClick = () => {
     setShowChats(true);
   }
-  
-  useEffect(()=>{
-    handleSampleData();
-  },[]);
 
-  const handleSampleData=async()=>{
-     setData(sampleData);
+  useEffect(() => {
+    handleSampleData();
+  }, []);
+
+  const handleSampleData = async () => {
+    setData(sampleData);
   }
 
   return (
+   // main app entry 
     <div className="app">
+
+      {/* heading part */}
       <div className='heading-part'>
-      <header>
-        <span className='brand-identity'>
-          <img className='brand-icon' src='./brain.svg' alt='DI-icon'/>
-          <p className='brand-text'> RON </p>
-        </span>
-        <div className='heading-button'>
-        <button className='dashboard-component-button'>Dashboard</button>
-        <button className='chat-component-button'>Chat</button>
-        </div>
-        <img className="profile-button" src="./profile.svg" alt="profile-icon" /> 
+        <header>
+          <span className='brand-identity'>
+            <img className='brand-icon' src='./brain.svg' alt='DI-icon' />
+            <p className='brand-text'> RON </p>
+          </span>
+          <div className='heading-button'>
+            <button className='dashboard-component-button'>Dashboard</button>
+            <button className='chat-component-button'>Chat</button>
+          </div>
+          <img className="profile-button" src="./profile.svg" alt="profile-icon" />
         </header>
       </div>
-     
-      <div className='Main-page-side-section'>
-        <div className='Menu'>
-          <div className={`Menu-item ${showChats ? 'selected' : ''}`} id="Menu-chats" onClick={handleChatClick}>
-            <span className="icon"><img src='./search.svg' alt='brain-wave-icon' /></span>
-            <span className="text">search here..</span>
-          </div>
 
-
-      {/* sample data section  */}
-       <div className='Sample-data-loading-section'>
-          {data.map((curr,index) => (
-          <div className='Sample-data'>
-          <p key={index}>{index+1}.</p>
-          <p>{curr}</p>
-          </div>
-
+      {/* main page left section - display list of industries */}
+      <div className='main-page-left-section'>
+        {/* sample data section  */}
+        <div className='Sample-data-loading-section'>
+          {data.map((curr, index) => (
+            <div className='Sample-data'>
+              <img className="page-icon" src="./page.svg" alt="page-icon"/>
+              <p className='data'>{curr}</p>
+              <img className="expand-button" src="./add-circle-button.svg" alt="expand-button"/>
+            </div>
           ))}
-       </div>
-       
-
-          {/* <div className={`Menu-item ${showSearch ? 'selected' : ''}`} id="Menu-search">
-            <span className="icon"><img src='./search.svg' alt='brain-wave-icon' /></span>
-            <span className="text">Search</span>
-          </div>
-          <div className={`Menu-item ${showSubscription ? 'selected' : ''}`} id="Menu-subscription">
-            <span className="icon"><img src='./subscription.svg' alt='brain-wave-icon' /></span>
-            <span className="text">Manage Subscription</span>
-          </div>
-          <div className={`Menu-item ${showFaq ? 'selected' : ''}`} id="Menu-update">
-            <span className="icon"><img src='./faq.svg' alt='brain-wave-icon' /></span>
-            <span className="text">Update & FAQ</span>
-          </div>
-          <div className={`Menu-item ${showSettings ? 'selected' : ''}`} id="Menu-setting">
-            <span className="icon"><img src='./settings.svg' alt='brain-wave-icon' /></span>
-            <span className="text">Settings</span>
-          </div> */}
+          
         </div>
-        {/* <div className='Horizontal-line'></div> */}
-        
+
       </div>
 
       <div className="Chat-component">
--- src/utils/sampleData.js
+++ src/utils/sampleData.js
@@ -1,19 +1,16 @@
- const sampleData=["Ron Frontend UI Design",
-"Create Ron Website Interface",
-"Develop Ron Frontend Components",
-"Design User Experience for Ron",
-"Implement Ron's Frontend Features",
-"Craft Ron's User-Friendly Interface",
-"Build Responsive Frontend for Ron",
-"User Interface Development for Ron",
-"Design and Code Ron's Website UI",
-"Frontend Implementation: Project Ron","Create Ron Website Interface",
-"Develop Ron Frontend Components",
-"Design User Experience for Ron",
-"Implement Ron's Frontend Features",
-"Craft Ron's User-Friendly Interface",
-"Build Responsive Frontend for Ron",
-"User Interface Development for Ron",
-"Design and Code Ron's Website UI"];
+ const sampleData=[
+    "Education",
+      "Hi-Tech",
+      "Power & Utility",
+      "Real-state",
+      "Retail/FMCG",
+      "Transportation",
+      "Education",
+      "Hi-Tech",
+      "Power & Utility",
+      "Real-state",
+      "Retail/FMCG",
+      "Transportation"        
+];
 
 export default sampleData;
