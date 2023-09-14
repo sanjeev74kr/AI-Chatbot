8d2ae50ea71ae0295ff48dc3a015274f22a22817
Sanjeev Kumar <dq9nakf6qpb13jzofo4xv163f3wdmd@bots.bitbucket.org>
2023-09-14 17:28:06 +0530

    changed delete confirmation appearence

--- src/components/DeleteConfirmation/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation/DeleteConfirmation.jsx
@@ -7,7 +7,7 @@ function DeleteConfirmation({ date, index, handleDelete, onCancel }) {
         <div className="delete-confirmation-dialogue-container">
             <p className='delete-confirmation-heading'>Delete chat?</p>
             <p className="delete-confirmation-msg">
-            <span>You'll no longer see this chat here. This will also delete related activity like prompts, responses and feedback from your Bard activity.</span>
+            <span>You'll no longer see this chat here. This will also delete related activity like prompts, responses and feedback from your RON activity.</span>
             {/* <span className='learn-more-link'><a href=''>Learn more</a></span>  */}
             </p>
             <div className="delete-confirmation-button-container">
